d09e9828276eca773f8f03ed9756072523140caf
emmanuel-nwafor <emitexc.e.o1@gmail.com>
2026-03-21 12:52:36 +0100

    feat: Updated wallet functionalities.

--- src/routes/payment.ts
+++ src/routes/payment.ts
@@ -9,6 +9,16 @@ const paymentRouter = Router();
 
 paymentRouter.get('/initialize/booking/:bookingId', initializeValidator, asyncHandler(Controller.initializeBookingPayment));
 paymentRouter.get('/initialize/booking/refund/:bookingId', initializeValidator, asyncHandler(Controller.bookingRefund));
+paymentRouter.get('/flw/callback', (req, res) => {
+    res.send(`
+        <html>
+            <head><title>Processing Payment...</title></head>
+            <body style="display:flex;justify-content:center;align-items:center;height:100vh;font-family:sans-serif;background-color:#ffffff;">
+                <h2 style="color:#003b14;">Payment completed. Redirecting to app...</h2>
+            </body>
+        </html>
+    `);
+});
 paymentRouter.get('/verify/:reference',verifyJWT([UserType.USER]), asyncHandler(Controller.verifyFlwTransaction));
 paymentRouter.post('/withdraw',withdrawValidator, asyncHandler(Controller.withdraw));
 
