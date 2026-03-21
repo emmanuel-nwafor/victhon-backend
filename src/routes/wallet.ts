f62df76f8c978371afe32d0d6c90c56d0feccf78
emmanuel-nwafor <emitexc.e.o1@gmail.com>
2026-03-21 10:34:24 +0100

    feat: Add wallet routes for balance, history, and specific transaction details.

--- src/routes/wallet.ts
+++ src/routes/wallet.ts
@@ -6,8 +6,8 @@ const wallet = Router();
 
 // wallet.post("/", create, asyncHandler(Controller.create));
 wallet.get("/", asyncHandler(Controller.wallet));
-wallet.get("/:transactionId", asyncHandler(Controller.transaction));
 wallet.get("/history", asyncHandler(Controller.history));
+wallet.get("/:transactionId", asyncHandler(Controller.transaction));
 
 
 export default wallet;
